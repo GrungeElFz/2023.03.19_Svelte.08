8b12552b28262349d1f49e5e1d6905e79527d87d
GrungeElFz <elf0114_@hotmail.com>
2023-03-19 23:37:51 -0500

    Initialized:    Clone from `git@github.com:GrungeElFz/2023.03.18_Svelte.07.git` repository

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import tippy from './lib/actions/tippy';
+
+	let content = "Test"
+</script>
+
+<input bind:value={content} />
+
+<button use:tippy={{ content, placement: 'right', theme: 'light' }}>Button</button>
+
+<style>
+</style>
