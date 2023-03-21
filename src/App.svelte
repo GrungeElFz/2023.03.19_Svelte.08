c909372cf7d1309420dd06d688299d8950b7aa44
GrungeElFz <elf0114_@hotmail.com>
2023-03-20 23:50:45 -0500

    Modified:   Cleared the entire file (src.App.svelte) Added:    An empty file for testing a Mnini Rounter (src/lib/pages/Home.svelte) Added:    An empty file for testing a Mnini Rounter (src/lib/pages/Settings.svelte)

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,5 @@
 <script>
-	import tippy from './lib/actions/tippy';
-
-	let content = "Test"
 </script>
-
-<input bind:value={content} />
-
-<button use:tippy={{ content, placement: 'right', theme: 'light' }}>Button</button>
 
 <style>
 </style>
