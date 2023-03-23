9f585884318807414e94795a1e59f0d366723b6c
GrungeElFz <elf0114_@hotmail.com>
2023-03-22 20:37:38 -0500

    Modified:   Imported { onMount } from 'svelte' (src/App.svelte)
                In a function `onRouteChange()`
                - Added an if...else if statement for each condition of page routing
                - Added `onMount(onRouteChange)` for the first time landing or refresh

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
+	import { onMount } from 'svelte';
 	import Home from './lib/pages/Home.svelte';
 	import Settings from './lib/pages/Settings.svelte';
 
 	let page;
 
 	function onRouteChange() {
-		console.log(window.location.hash.slice(1));
+		const path = console.log(window.location.hash.slice(1));
+
+		if (page === '/') {
+			page = 'home';
+		} else if (page === '/settings') {
+			page = 'settings';
+		} else {
+			window.location.hash = '/';
+		}
+
+		onMount(onRouteChange);
 	}
 </script>
 
