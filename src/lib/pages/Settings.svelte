aac8f944f90953f3c60f0c9bd768f7d87d2bcfeb
GrungeElFz <elf0114_@hotmail.com>
2023-03-24 22:59:00 -0500

    Modified:   Assigned a const variable `unsubsribe` to `settings.subscribe(...)` (src/lib/pages/Home.svelte)             Imported { onDestroy } from 'svelte'             Added `onDestroy(unsubsribe)`
    Modified:   Assigned a const variable `unsubsribe` to `settings.subscribe(...)` (src/lib/pages/Settings.svelte)
                Imported { onDestroy } from 'svelte'
                Added `onDestroy(unsubsribe)`

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,15 +1,18 @@
 <script>
+	import { onDestroy } from 'svelte';
 	import settings from '../stores/settings';
 
 	let colorScheme;
 
-	settings.subscribe((settings) => {
+	const unsubscribe = settings.subscribe((settings) => {
 		colorScheme = settings.colorScheme;
 	});
 
 	$: {
 		settings.update((prevSettings) => ({ ...prevSettings, colorScheme }));
 	}
+
+	onDestroy(unsubscribe);
 </script>
 
 <h2>Settings</h2>
