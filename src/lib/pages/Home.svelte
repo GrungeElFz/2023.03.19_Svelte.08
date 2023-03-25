4915c71a95b7ee687ea499aebf7d5892176a147b
GrungeElFz <elf0114_@hotmail.com>
2023-03-24 22:47:41 -0500

    Modified:   Imported settings from '../stores/settings' (src/lib/pages/Home.svelte)             Declared a let variable `colorScheme`             Added `settings.subscribe(...)`             Added `{colorScheme}`
    Modified:   Imported settings from '../stores/settings' (src/lib/pages/Settings.svelte)
                Declared a let variable `colorScheme`
                Added `settings.subscribe(...)`
                Added a reactive statement to track `settings.update(...)`
    
                Added 2 labels, Dark and Light
                Added <input> in each labels
                Added a bind directive `group={colorScheme}` in each <input>

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,1 +1,13 @@
+<script>
+	import settings from '../stores/settings';
+
+	let colorScheme;
+
+	settings.subscribe((settings) => {
+		colorScheme = settings.colorScheme;
+	});
+</script>
+
 <h2>Home</h2>
+
+{colorScheme}
--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,1 +1,25 @@
+<script>
+	import settings from '../stores/settings';
+
+	let colorScheme;
+
+	settings.subscribe((settings) => {
+		colorScheme = settings.colorScheme;
+	});
+
+	$: {
+		settings.update((prevSettings) => ({ ...prevSettings, colorScheme }));
+	}
+</script>
+
 <h2>Settings</h2>
+
+<label>
+	<input bind:group={colorScheme} type="radio" name="colorScheme" value="dark" />
+	Dark
+</label>
+
+<label>
+	<input bind:group={colorScheme} type="radio" name="colorScheme" value="light" />
+	Light
+</label>
