22dccfff5b2c02fa1d27846230d9c4f63fc94b0b
GrungeElFz <elf0114_@hotmail.com>
2023-03-25 21:35:42 -0500

    Modified:   Replaced a bind directive's group value from `colorScheme` to `$settings.colorScheme`(src/lib/pages/Settings.svelte)             Added <p> to display a current theme             Added <br>             Re-arranged lines

--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -4,18 +4,22 @@
 
 <h2>Settings</h2>
 
-<button
-	on:click={() => {
-		$settings.colorScheme = $settings.colorScheme === 'dark' ? 'light' : 'dark';
-	}}>Toggle Color Scheme</button
->
-
-<!-- <label>
-	<input bind:group={colorScheme} type="radio" name="colorScheme" value="dark" />
+<label>
+  <input bind:group={$settings.colorScheme} type="radio" name="colorScheme" value="dark" />
 	Dark
 </label>
 
 <label>
-	<input bind:group={colorScheme} type="radio" name="colorScheme" value="light" />
+  <input bind:group={$settings.colorScheme} type="radio" name="colorScheme" value="light" />
 	Light
-</label> -->
+</label>
+
+<br>
+
+<button
+on:click={() => {
+  $settings.colorScheme = $settings.colorScheme === 'dark' ? 'light' : 'dark';
+}}>Toggle Color Scheme</button
+>
+
+<p>Current Theme: {$settings.colorScheme}</p>
