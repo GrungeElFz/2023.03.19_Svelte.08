a742f3bc3c6ea84ed30b60d017eabde7ce042828
GrungeElFz <elf0114_@hotmail.com>
2023-03-26 08:56:48 -0500

    Modified:   Added <svelte:head> (src/App.svelte)             Added an 'IBM Plex Mono' font in the head
    Modified:   In :root selector (src/app.css)
                - Changed `background-color` to `--backgroundColor`
                - Changed `color` to `--color`
    
                Added body selector
                - Added `background-color` prop, assigned to `var(--backgroundColor)`
                - Added `color` prop, assigned to `var(--color)`
                - Added `font-family` prop, assigned to `'IBM Plex Mono', monospace`

--- src/App.svelte
+++ src/App.svelte
@@ -27,6 +27,15 @@
 	<a href="#/settings">Settings</a>
 </nav>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;300;400;500;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 {#if page === 'home'}
 	<Home />
 {:else if page === 'settings'}
